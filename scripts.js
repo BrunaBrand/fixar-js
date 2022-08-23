4707cf0cf8668f82861fe063d3fd5e90bbd067e4
Bruna Brandao <bruna.costab@hotmail.com>
2022-08-22 21:22:57 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// 1. Declare uma variável de nome weight
+
+// let weight
+
+// 2. Que tipo de dado é a variável acima?
+// console.log(typeof weight)
+
+/*
+   3. Declare uma variável e atribua valores para cada um dos dados:
+
+   * name: String
+   * age: Number (integer)
+   * stars: Number (float)
+   * isSubscribed: Boolean
+
+*/
+
+// let name = 'Bruna'
+// let age = 25
+// let stars = 4.8
+// let isSubscribed = true
+
+/*
+   4. A variável student abaixp é de que tipo de dado?
+
+   
+   4.1 Atribua a ela as mesmas propriedades e valores do exercício 3.
+   
+   4.2 Mostre no console a seguinte mensagem:
+   <name> de idade <age> pesa <weight> kg.
+
+*/
+let student = {
+  name: 'Bruna',
+  age: 25,
+  weight: 61
+}
+
+console.log(
+  `${student.name} de idade ${student.age} pesa ${student.weight} kg.`
+)
+
+/*
+   5. Declare uma variável do tipo Array, de nome studentes e atribua a ela nenhum valor, ou seja, somente o Array vazio
+ */
